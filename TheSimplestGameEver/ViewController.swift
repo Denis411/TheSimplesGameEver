db3135e5affbf43fa640b9b646f90617fe54a435
DennisNewMachine <dnsljnv411@gmail.com>
2022-04-16 09:36:43 +0300

    Initial Commit

---
+++ TheSimplestGameEver/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheSimplestGameEver
+//
+//  Created by Dennis Loskutov on 16.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
