d23b6233a9638d1aded3e309c7777af0823e2a0c
DennisNewMachine <dnsljnv411@gmail.com>
2022-04-16 10:11:13 +0300

    Create custom button

---
+++ TheSimplestGameEver/CustomBlueButton.swift
@@ -0,0 +1,37 @@
+//
+//  CustomBlueButton.swift
+//  TheSimplestGameEver
+//
+//  Created by Dennis Loskutov on 16.04.2022.
+//
+
+import UIKit
+
+class CustomBlueButton: UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUpAppearance()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("I don't use storyboards in this project.")
+    }
+    
+    private func setUpAppearance() {
+        self.layer.backgroundColor = UIColor.blue.cgColor
+        self.layer.cornerRadius = 15
+        self.setTitleColor(.white, for: .normal)
+        self.setTitleColor(.gray, for: .highlighted)
+    }
+    
+    func setUpBottomConstraints(for controller: UIViewController) {
+        controller.view.addSubview(self)
+        self.translatesAutoresizingMaskIntoConstraints = false
+        self.leadingAnchor.constraint(equalTo: controller.view.leadingAnchor, constant: 25).isActive = true
+        self.trailingAnchor.constraint(equalTo: controller.view.trailingAnchor, constant: -25).isActive = true
+        self.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        self.centerXAnchor.constraint(equalTo: controller.view.centerXAnchor).isActive = true
+        self.bottomAnchor.constraint(equalTo: controller.view.bottomAnchor, constant: -25).isActive = true
+    }
+}
--- TheSimplestGameEver/ViewController.swift
+++ TheSimplestGameEver/ViewController.swift
@@ -8,10 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let button = CustomBlueButton()
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .green
+        button.setUpBottomConstraints(for: self)
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        button.layoutSubviews()
     }
 }
 
