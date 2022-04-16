d3f9f3e21c129d87ba8a396030a4a3b2f5549434
DennisNewMachine <dnsljnv411@gmail.com>
2022-04-16 11:44:24 +0300

    Prepare start view controller

--- TheSimplestGameEver/Screens/ViewController.swift
+++ TheSimplestGameEver/Screens/ViewController.swift
@@ -8,16 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-    let button = CustomBlueButton()
+    let gameNameTitle: CenteredCustomLabel = {
+        let label = CenteredCustomLabel()
+        let title = NSLocalizedString("game_name", comment: "")
+        label.text = title
+        return label
+    }()
+    
+    let startButton: CustomBlueButton = {
+        let button = CustomBlueButton()
+        let title = NSLocalizedString("start_new_game", comment: "")
+        button.setTitle(title, for: .normal)
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .green
-        button.setUpBottomConstraints(for: self)
+        startButton.setUpBottomConstraints(for: self)
+        setUpGameNameTitleConstraints()
+        startButton.addTarget(self, action: #selector(openNextScreen), for: .touchUpInside)
     }
     
-    override func viewWillLayoutSubviews() {
-        super.viewWillLayoutSubviews()
-        button.layoutSubviews()
+    private func setUpGameNameTitleConstraints() {
+        view.addSubview(gameNameTitle)
+        gameNameTitle.translatesAutoresizingMaskIntoConstraints = false
+        gameNameTitle.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+        gameNameTitle.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
+        gameNameTitle.topAnchor.constraint(equalTo: view.topAnchor, constant: 50).isActive = true
+        gameNameTitle.heightAnchor.constraint(equalToConstant: 75).isActive = true
+    }
+    
+    @objc private func openNextScreen() {
+        
     }
 }
 
