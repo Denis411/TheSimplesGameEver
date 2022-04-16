6601b28c6d9d1ffb86cdc7b3ca2a132d81d16586
DennisNewMachine <dnsljnv411@gmail.com>
2022-04-16 15:07:16 +0300

    Create router

---
+++ TheSimplestGameEver/MainRouter.swift
@@ -0,0 +1,35 @@
+//
+//  Router.swift
+//  TheSimplestGameEver
+//
+//  Created by Dennis Loskutov on 16.04.2022.
+//
+
+import UIKit
+
+class MainRouter : MainRouterProtocol {
+    var dataContainer: DataContainerProtocol
+    var childrenRouters = [MainRouterProtocol]()
+    var navController: UINavigationController
+    
+    init(navController: UINavigationController, dataContainer: DataContainerProtocol) {
+        self.navController = navController
+        self.dataContainer = dataContainer
+    }
+    
+    func pushStartVC() {
+        let startVC = StartViewController()
+        startVC.router = self
+        navController.pushViewController(startVC, animated: false)
+    }
+    
+    func pushPlayerThinkingVC() {
+        let playerThinkingVC = PlayerThinkingOfNumberViewController()
+        playerThinkingVC.router = self
+        navController.pushViewController(playerThinkingVC, animated: true)
+    }
+    
+    func pushThirdVC() {
+
+    }
+}
---
+++ TheSimplestGameEver/MainRouterProtocol.swift
@@ -0,0 +1,18 @@
+//
+//  RouterProtocol.swift
+//  TheSimplestGameEver
+//
+//  Created by Dennis Loskutov on 16.04.2022.
+//
+
+import UIKit
+
+protocol MainRouterProtocol: AnyObject {
+    var dataContainer: DataContainerProtocol { get set }
+    var childrenRouters: [MainRouterProtocol] { get set }
+    var navController: UINavigationController { get set }
+    
+    func pushStartVC()
+    func pushPlayerThinkingVC()
+    func pushThirdVC()
+}
