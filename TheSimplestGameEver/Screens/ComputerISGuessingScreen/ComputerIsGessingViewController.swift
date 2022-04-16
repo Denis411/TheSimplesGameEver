691a0260f3e7510d1344ea4c886b63eda4afcc67
DennisNewMachine <dnsljnv411@gmail.com>
2022-04-16 19:06:36 +0300

    Create new vc

---
+++ TheSimplestGameEver/Screens/ComputerISGuessingScreen/ComputerIsGessingViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ComputerIsGessingViewController.swift
+//  TheSimplestGameEver
+//
+//  Created by Dennis Loskutov on 16.04.2022.
+//
+
+import UIKit
+
+class ComputerIsGessingViewController: UIViewController {
+    weak var router: MainRouterProtocol?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .blue
+    }
+    
+    
+    
+    
+}
+
+// inits
+extension ComputerIsGessingViewController {
+    
+}
