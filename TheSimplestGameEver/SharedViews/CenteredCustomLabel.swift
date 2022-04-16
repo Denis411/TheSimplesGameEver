4bbc54eb92f5ad8dbf8fc1eedfecafc6f770cc43
DennisNewMachine <dnsljnv411@gmail.com>
2022-04-16 11:26:56 +0300

    Create custom label
    Add custom label

---
+++ TheSimplestGameEver/SharedViews/CenteredCustomLabel.swift
@@ -0,0 +1,29 @@
+//
+//  CenteredCustomLabel.swift
+//  TheSimplestGameEver
+//
+//  Created by Dennis Loskutov on 16.04.2022.
+//
+
+import UIKit
+
+class CenteredCustomLabel: UILabel {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUpStyle()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("I don't use storyboards in the project")
+    }
+    
+    private func setUpStyle() {
+        self.backgroundColor = .clear
+        self.textColor = .blue
+        self.textAlignment = .center
+        self.font = UIFont(name: "GillSans-BoldItalic", size: 20)
+        self.numberOfLines = 1
+        self.lineBreakMode = .byTruncatingTail
+    }
+}
